e96653de8a3b6b408063dcffb00d8d0919400b69
beniroquai <bene.d@gmx.de>
2025-08-29 12:38:12 +0200

    Update DemoController.py

--- imswitch/imcontrol/controller/controllers/DemoController.py
+++ imswitch/imcontrol/controller/controllers/DemoController.py
@@ -306,8 +306,7 @@ class DemoController(ImConWidgetController):
         try:
             if self.stages:
                 # Move to position
-                self.stages.move(position[0], "X", is_absolute=True, speed=self.params.maxSpeed)
-                self.stages.move(position[1], "Y", is_absolute=True, speed=self.params.maxSpeed)
+                self.stages.move(position[0], "XY", is_absolute=True, speed=self.params.maxSpeed, is_blocking=True)
                 self._logger.debug(f"Moved to position: {position}")
             else:
                 self._logger.warning("No stages available for movement")

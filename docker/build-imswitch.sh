a09f406ba5a6dcbea0d575f0026a5fcf531ae804
beniroquai <bene.d@gmx.de>
2025-11-18 14:11:16 +0100

    add picamera2 to docker

--- docker/build-imswitch.sh
+++ docker/build-imswitch.sh
@@ -1,5 +1,42 @@
 #!/usr/bin/env -S bash -eux
 
+# Install pip and picamera2 for Raspberry Pi support
+# Note: picamera2 requires system libcamera libraries
+apt update && apt install -y --no-install-recommends \
+         python3-pip \
+         python3-picamera2 \
+         python3-libcamera \
+         libcamera0
+
+# Create symlinks to make system picamera2 and libcamera accessible from conda environment
+# This is necessary because picamera2 relies on system-installed libcamera binaries
+CONDA_SITE_PACKAGES="/opt/conda/envs/imswitch/lib/python3.11/site-packages"
+SYSTEM_SITE_PACKAGES="/usr/lib/python3/dist-packages"
+
+mkdir -p "$CONDA_SITE_PACKAGES"
+
+# Symlink picamera2 module
+if [[ -d "$SYSTEM_SITE_PACKAGES/picamera2" ]]; then
+    ln -sf "$SYSTEM_SITE_PACKAGES/picamera2" "$CONDA_SITE_PACKAGES/picamera2"
+fi
+
+# Symlink libcamera module
+if [[ -d "$SYSTEM_SITE_PACKAGES/libcamera" ]]; then
+    ln -sf "$SYSTEM_SITE_PACKAGES/libcamera" "$CONDA_SITE_PACKAGES/libcamera"
+fi
+
+# Symlink _libcamera (the C extension)
+if [[ -f "$SYSTEM_SITE_PACKAGES/_libcamera.so" ]]; then
+    ln -sf "$SYSTEM_SITE_PACKAGES/_libcamera.so" "$CONDA_SITE_PACKAGES/_libcamera.so"
+fi
+
+# Also symlink the .so files if they exist in different locations
+for libcam_so in "$SYSTEM_SITE_PACKAGES"/libcamera*.so; do
+    if [[ -f "$libcam_so" ]]; then
+        ln -sf "$libcam_so" "$CONDA_SITE_PACKAGES/$(basename "$libcam_so")"
+    fi
+done
+
 # Install ImSwitch from our local copy of the repo
 cp -r /mnt/ImSwitch /tmp/ImSwitch
 cd /tmp/ImSwitch
